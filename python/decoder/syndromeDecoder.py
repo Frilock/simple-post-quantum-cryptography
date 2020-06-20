f1ecd9ecfd51a012937ab9c00065aa2c278f278e
ivan <uvan.korotaev@yandex.ru>
2020-06-20 22:47:09 +0300

    refactoring

--- python/decoder/syndromeDecoder.py
+++ python/decoder/syndromeDecoder.py
@@ -1,42 +1,44 @@
 from python.linearCode import LinearCode
+import numpy as np
+import matplotlib.pyplot as plot
 import python.utils as utils
 
 
-def syndrome_decoding_analysis(lc):
-    syndrome_table = lc.get_syndrome_table()
+def syndrome_decoding_analysis(linear_code):
+    syndrome_table = linear_code.get_syndrome_table()
     bit_errors = np.arange(0, 1, 0.1)
     errors = []
     rejections = []
-    plt.figure()
-    plt.xlabel('bit error')
-    plt.ylabel('errors amount')
 
     for bit_error in bit_errors:
         error_count = 0
         rejection_count = 0
     
-        for codeword in lc.codewords:
-            error = lc.get_error_vector(bit_error)
-            received = (codeword + error) % lc.q
-            syndrome = lc.get_syndrome(received)
-            syndrome_str = lc.vector_to_str(syndrome)
+        for codeword in linear_code.codewords:
+            error = linear_code.get_error_vector(bit_error)
+            received = (codeword + error) % linear_code.q
+            syndrome = linear_code.get_syndrome(received)
+            syndrome_str = utils.vector_to_str(syndrome)
             if syndrome_str in syndrome_table:
                 error_pattern = syndrome_table[syndrome_str]
-                decoded = (received + error_pattern) % lc.q
+                decoded = (received + error_pattern) % linear_code.q
                 if not np.array_equal(decoded, codeword):
                     error_count += 1
-            else:
-                # Синдрома нет в таблице - отказ от декодирования
+            else:  # Синдрома нет в таблице - отказ от декодирования
                 rejection_count += 1
         errors.append(error_count)
         rejections.append(rejection_count)
-    plt.plot(bit_errors, errors, label='errors')
-    plt.plot(bit_errors, rejections, label='rejections')
-    plt.legend()
-    plt.show()
+
+    plot.figure()
+    plot.xlabel('bit error')
+    plot.ylabel('errors amount')
+    plot.plot(bit_errors, errors, label='errors')
+    plot.plot(bit_errors, rejections, label='rejections')
+    plot.legend()
+    plot.show()
 
 
-lc = LinearCode(17, 7, 2)
-lc.print_params()
+linear_code = LinearCode(17, 7, 2)
+linear_code.print_params()
 
-syndrome_decoding_analysis(lc)
+syndrome_decoding_analysis(linear_code)
